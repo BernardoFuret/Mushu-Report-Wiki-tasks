870575a8ec28ac267987590dc7a8252378010711
BernardoFuret <bernardo.furet@hotmail.com>
2024-09-19 19:51:39 +0100

    Add example of processing the headers

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,22 @@ import { fileURLToPath } from 'node:url';
 import { parse } from 'csv-parse';
 
 import { getDataFilePath } from './helpers.js';
+import { processRecord } from './processor.js';
 
 const srcDirname = dirname(fileURLToPath(import.meta.url));
 
 const stream = createReadStream(getDataFilePath(srcDirname, 'example.csv')).pipe(parse());
 
+const headers: string[] = [];
+
 // eslint-disable-next-line no-restricted-syntax
 for await (const record of stream) {
   // eslint-disable-next-line no-console
   console.log('>>record', record);
+
+  if (!headers.length) {
+    headers.push(...record);
+  } else {
+    processRecord(record, headers);
+  }
 }
---
+++ src/processor.ts
@@ -0,0 +1,7 @@
+const processRecord = (record: string[], headers: string[]): void => {
+  // TODO
+  // eslint-disable-next-line no-console
+  console.log('>>processRecord', { record, headers });
+};
+
+export { processRecord };
