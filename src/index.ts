d37857c3439a50ac5793d80e65aca001bfa05477
BernardoFuret <bernardo.furet@hotmail.com>
2024-09-19 01:04:47 +0100

    Add CSV reader stream

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-// eslint-disable-next-line no-console
-console.log('Test');
+import { createReadStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { parse } from 'csv-parse';
+
+import { getDataFilePath } from './helpers.js';
+
+const srcDirname = dirname(fileURLToPath(import.meta.url));
+
+const stream = createReadStream(getDataFilePath(srcDirname, 'example.csv')).pipe(parse());
+
+// eslint-disable-next-line no-restricted-syntax
+for await (const record of stream) {
+  // eslint-disable-next-line no-console
+  console.log('>>record', record);
+}
